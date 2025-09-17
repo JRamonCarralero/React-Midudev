7396ad76870df76b2bf8c537565a593b5d8f00cd
JRamonCarralero <grukduz@gmail.com>
2025-09-17 17:07:43 +0200

    hola mundo

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+
+// eslint-disable-next-line react-refresh/only-export-components
+const Button = ({text}) => {
+  return (
+    <button>{text}</button>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.Fragment>
+    <Button text="button1" />
+    <Button text="button2" />
+    <Button text="button3" />
+  </React.Fragment>
 )
