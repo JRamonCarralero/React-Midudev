99e28df93933a81198c933f59335565aa7998588
JRamonCarralero <grukduz@gmail.com>
2025-09-24 12:55:34 +0200

    06 - Eliminado Prop drilling

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -2,36 +2,21 @@ import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { Products } from './components/Products'
 import { products as initialProducts } from './mocks/products'
-import { useContext, useState } from 'react'
+import { useState } from 'react'
 import { IS_DEVELOPMENT } from './config'
-import { FiltersContext } from './contexts/filters'
-
-function useFilters() {
-  const { filters, setFilters } = useContext(FiltersContext)
-
-  const filterProducts = (products) => {
-    return products.filter((product) => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category === 'all' || product.category === filters.category)
-      )
-    })
-  }
-
-  return { filters, setFilters, filterProducts }
-}
+import { useFilters } from './hooks/useFilters'
 
 function App() {
   const [products] = useState(initialProducts)
-  const { filters, setFilters, filterProducts } = useFilters()
+  const { filterProducts } = useFilters()
 
   const filteredProducts = filterProducts(products)
 
   return (
     <>
-      <Header changeFilters={setFilters} />
+      <Header />
       <Products products={filteredProducts} />
-      {IS_DEVELOPMENT && <Footer filters={filters} />} {/* Solo se muestra en modo desarrollo */}
+      {IS_DEVELOPMENT && <Footer />} {/* Solo se muestra en modo desarrollo */}
     </>
   )
 }
--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,31 +1,33 @@
 import './Filters.css'
-import { useState, useId } from 'react'
+import { useId } from 'react'
+import { useFilters } from '../hooks/useFilters'
 
-export function Filters ({ onChange}) {
-  const [minPrice, setMinPrice] = useState(0)
+
+export function Filters () {
+  const { filters, setFilters } = useFilters()
+
   // Como los hooks siempre se ejecutan en el mismo orden, podemos usar useId, que genera un id unico para cada hook
   const minPriceFilterId = useId()
   const categoryFilterId = useId()
 
   const handleChangeMinPrice = (event) => {
-    setMinPrice(event.target.value)
-    onChange(prevState => ({ ...prevState, minPrice: event.target.value }))
+    setFilters(prevState => ({ ...prevState, minPrice: event.target.value }))
   }
 
   const handleChangeCategory = () => {
-    onChange(prevState => ({ ...prevState, category: event.target.value }))
+    setFilters(prevState => ({ ...prevState, category: event.target.value }))
   }
 
   return (
     <section className='filters'>
       <div>
         <label htmlFor='price'>Precio a partir de</label>
-        <input id={minPriceFilterId} type='range' name='price' min='0' max='1000' onChange={handleChangeMinPrice} value={minPrice} />
-        <span>{minPrice}</span>
+        <input id={minPriceFilterId} type='range' name='price' min='0' max='1000' onChange={handleChangeMinPrice} value={filters.minPrice} />
+        <span>{filters.minPrice}</span>
       </div>
       <div>
         <label htmlFor='category'>Categoria</label>
-        <select id={categoryFilterId} name='category' onChange={handleChangeCategory}>
+        <select id={categoryFilterId} name='category' onChange={handleChangeCategory} value={filters.category}>
           <option value='all'>Todas</option>
           <option value='smartphones'>Smartphones</option>
           <option value='laptops'>Portátiles</option>
