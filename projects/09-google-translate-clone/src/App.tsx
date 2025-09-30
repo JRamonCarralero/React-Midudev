fcc378bbee436ebc0a761ff6862ac4046b9cae5e
JRamonCarralero <grukduz@gmail.com>
2025-09-30 13:06:21 +0200

    09 -Languages selectors

--- projects/09-google-translate-clone/src/App.tsx
+++ projects/09-google-translate-clone/src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useStore } from './hooks/useStore'
+
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import { AUTO_LANGUAGE } from './constants'
+import { ArrowsIcon } from './components/Icons'
+import { LanguageSelector } from './components/LanguageSelector'
+import { SectionType } from './types.d'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { fromLanguage, toLanguage, setFromLanguage, setToLanguage, interchangeLanguages } = useStore()
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container fluid>
+      <h1>Google Translate</h1>
+      
+      <Row>
+        <Col>
+          <h2>From</h2>
+          <LanguageSelector onChange={setFromLanguage} type={SectionType.From} value={fromLanguage} />
+        </Col>
+
+        <Col>
+          <Button variant="link" disabled={fromLanguage === AUTO_LANGUAGE} onClick={() => interchangeLanguages()}>
+            <ArrowsIcon />
+          </Button>
+        </Col>
+
+        <Col>
+          <h2>To</h2>
+          <LanguageSelector onChange={setToLanguage} type={SectionType.To} value={toLanguage} />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
