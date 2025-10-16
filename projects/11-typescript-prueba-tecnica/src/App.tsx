60414fdc7ed9053ad1409029cd9e1810c7089135
JRamonCarralero <grukduz@gmail.com>
2025-10-16 13:24:14 +0200

    11 - colorear, ordenar, borrar, resetear

--- projects/11-typescript-prueba-tecnica/src/App.tsx
+++ projects/11-typescript-prueba-tecnica/src/App.tsx
@@ -1,21 +1,58 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { UsersList } from './components/UserList'
+import { type User } from './types'
 
 function App() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState<User[]>([])
+  const [showColors, setShowColors] = useState(false)
+  const [sortByCountry, setSortByCountry] = useState(false)
+  const originalUsers = useRef<User[]>([])
+
+  const toggleColors = () => {
+    setShowColors(!showColors)
+  }
+
+  const toggleSortByCountry = () => {
+    setSortByCountry(prevState => !prevState)
+  }
+
+  const handleDelete = (email: string) => {
+    const filteredUsers = users.filter(user => user.email !== email)
+    setUsers(filteredUsers)        
+  }
+
+  const handleReset = () => {
+    setUsers(originalUsers.current)
+  } 
 
   useEffect(() => {
     fetch('https://randomuser.me/api/?results=100')
       .then(res => res.json())
-      .then(data => setUsers(data.results))
+      .then(data => {
+        setUsers(data.results)
+        originalUsers.current = data.results
+      })
       .catch(err => console.log(err))
   }, [])
+
+  const sortedUsers = sortByCountry ? users.toSorted((a, b) => a.location.country.localeCompare(b.location.country)) : users
 
   return (
     <div className='App'>
       <h1>Prueba técnica</h1>
-      <UsersList users={users} />
+      <header>
+        <button onClick={toggleColors}>
+          Colorear filas
+        </button>
+        <button onClick={toggleSortByCountry}>
+          { sortByCountry ? 'No ordenar por países' : 'Ordenar por país' }
+        </button>
+        <button onClick={handleReset}>
+          Resetear
+        </button>
+      </header>
+      <UsersList showColors={showColors} onDelete={handleDelete} users={sortedUsers} />
     </div>
   )
 }
